725e69b69319c0316969424d87e8d40c39ef7b78
Kuba <kubaklicki99@gmail.com>
2026-02-17 21:00:51 +0100

    Adding program for random number in arrays

---
+++ Exercise_1.3_arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace randomArrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[10];
+        Random rnd = new Random();
+
+        for(int i = 0; i < 10; i++)
+        {
+            int randomNumber = rnd.Next(-10, 10);
+            numbers[i] = randomNumber;
+            Console.Write(numbers[i] + " ");
+        }
+    }
+}
