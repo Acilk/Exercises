65bacc56f5ed17676a7cec83668fdf61f8349bbf
Kuba <kubaklicki99@gmail.com>
2026-02-18 19:20:23 +0100

    Adding done exercise

---
+++ Exercise_1.3_arrays/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace howManyNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[20];
+        int[] appearedNumber = new int[11];
+        Random rnd = new Random();
+        
+        Console.WriteLine("Drawn numbers:");
+        for(int i = 0; i < 20; i++)
+        {
+            int randomNumber = rnd.Next(1, 11);
+            numbers[i] = randomNumber;
+
+            Console.Write(numbers[i] + " ");
+
+            for(int j = 0; j <= 10; j++)
+            {
+                if(j == numbers[i])
+                {
+                    appearedNumber[j] += 1;
+                }
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Appeard of number:");
+        for(int i = 1; i < 11; i++)
+        {
+            Console.WriteLine(i + "-" + appearedNumber[i]);
+        }
+
+    }
+}
