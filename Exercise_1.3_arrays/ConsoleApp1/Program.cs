5208e65c0f6b83a7a535105e2af9ea3e9df1dbb1
Kuba <kubaklicki99@gmail.com>
2026-02-18 18:13:51 +0100

    Adding done exercise

--- Exercise_1.3_arrays/ConsoleApp1/Program.cs
+++ Exercise_1.3_arrays/ConsoleApp1/Program.cs
@@ -6,14 +6,52 @@ class Program
 {
     static void Main(string[] args)
     {
+        int min = 0, max = 0, mean = 0, sum = 0, greater = 0, smaller = 0;
         int[] numbers = new int[10];
         Random rnd = new Random();
 
+        Console.WriteLine("Drawn numbers:");
         for(int i = 0; i < 10; i++)
         {
             int randomNumber = rnd.Next(-10, 10);
             numbers[i] = randomNumber;
+            sum += numbers[i];
             Console.Write(numbers[i] + " ");
         }
+
+        mean = sum / 10;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if(numbers[i] <= min)
+            {
+                min = numbers[i];
+            }
+            else if(numbers[i] > min && numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+
+            if(numbers[i] < mean)
+            {
+                smaller++;
+            }
+            else
+                greater++;
+        }
+
+        Array.Reverse(numbers);
+
+        Console.WriteLine();
+        Console.WriteLine("Min: " + min + ", Max:  " + max);
+        Console.WriteLine("Mean: " + mean);
+        Console.WriteLine("Smaller than mean: " + smaller);
+        Console.WriteLine("Greater than mean: " + greater);
+        Console.WriteLine("Numbers in reverse order: ");
+        foreach (int l in numbers)
+        {
+            Console.Write(l + " ");
+        }
+
     }
 }
--- Exercise_1.3_arrays/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ Exercise_1.3_arrays/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f62e72a88f1ee271cc9f06af8fc0e7482de4756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725e69b69319c0316969424d87e8d40c39ef7b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
