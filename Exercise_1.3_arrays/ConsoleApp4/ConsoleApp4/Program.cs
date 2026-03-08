765c6ad01eaddd8429c8f27ff7cb06ae4d82cf3f
Kuba <kubaklicki99@gmail.com>
2026-03-08 19:18:42 +0100

    Program checks the last char and counting appearance of this char

--- Exercise_1.3_arrays/ConsoleApp4/ConsoleApp4/Program.cs
+++ Exercise_1.3_arrays/ConsoleApp4/ConsoleApp4/Program.cs
@@ -13,21 +13,21 @@ class Program
 
         Console.WriteLine("Please, enter a integer to binary convert: ");
 
-        while(!int.TryParse(Console.ReadLine(), out number))
+        while (!int.TryParse(Console.ReadLine(), out number))
         {
             Console.WriteLine("Invalid integer, please try again! ");
         }
 
         int n = Math.Abs(number);
 
-       
-        
-        for(int i = 0; i < 32; i++)
+
+
+        for (int i = 0; i < 32; i++)
         {
             binaryNumbers[i] = n % 2;
             n /= 2;
         }
-        
+
 
 
         Console.Write("ZM: ");
@@ -35,17 +35,17 @@ class Program
         {
             Console.Write("0.");
         }
-        else if(number < 0)
+        else if (number < 0)
         {
             Console.Write("1.");
         }
 
-        for(int i = 31; i >= 0; i--)
+        for (int i = 31; i >= 0; i--)
         {
             Console.Write(binaryNumbers[i]);
         }
-        
-        if(number < 0)
+
+        if (number < 0)
         {
             int[] zu1 = new int[32];
             int[] zu2 = new int[32];
@@ -55,11 +55,11 @@ class Program
 
             for (int i = 0; i < 32; i++)
             {
-                if(zu1[i] == 0)
+                if (zu1[i] == 0)
                 {
                     zu1[i] = 1;
                 }
-                else if(zu1[i] == 1)
+                else if (zu1[i] == 1)
                 {
                     zu1[i] = 0;
                 }
@@ -72,14 +72,14 @@ class Program
                 Console.Write(zu1[i]);
             }
 
-            for(int i = 0; i < 32; i++)
+            for (int i = 0; i < 32; i++)
             {
                 zu2[i] = zu1[i];
             }
 
-            for(int i = 0; i < 32; i++)
+            for (int i = 0; i < 32; i++)
             {
-                if(zu2[i] == 0)
+                if (zu2[i] == 0)
                 {
                     zu2[i] = 1;
                     break;
@@ -92,7 +92,7 @@ class Program
 
             Console.WriteLine();
             Console.Write("ZU2:  ");
-            for(int i = 31; i >= 0; i--)
+            for (int i = 31; i >= 0; i--)
             {
                 Console.Write(zu2[i]);
             }
---
+++ Exercise_1.3_arrays/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 0;
+
+        while(!int.TryParse(Console.ReadLine(), out n) && n > 0)
+        {
+            Console.WriteLine("Invalid integer, please try again! ");
+        }
+
+        bool[,] a = new bool[n,n];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(nwd(i + 1, j + 1) == 1)
+                {
+                    a[i,j] = true;
+                }
+                else
+                    a[i,j] = false;
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(a[i,j])
+                {
+                    Console.Write("+ ");
+                }
+                else
+                    Console.Write(". ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+
+    static int nwd(int a, int b)
+    {
+        while(b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
