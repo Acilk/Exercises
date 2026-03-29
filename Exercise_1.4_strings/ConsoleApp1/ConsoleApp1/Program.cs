dac50de3c47af4775f683a71ca3fde17c88282ee
Kuba <kubaklicki99@gmail.com>
2026-03-29 20:49:01 +0200

    next

---
+++ Exercise_1.4_strings/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LastChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] lastChar = input.ToCharArray();
+            char lastLetter = lastChar[lastChar.Length - 1];
+            int numberOfLC = 0;
+       
+            foreach (char c in lastChar)
+            {  
+                Console.WriteLine(c); 
+            }
+
+            foreach (char c in lastChar)
+            {
+                if (lastLetter == c)
+                {
+                    numberOfLC++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(numberOfLC - 1);
+        }
+    }
+}
